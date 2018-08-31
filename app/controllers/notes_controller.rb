56ba42f1a23e1bff001e2db5388047780b801c71
Nate <nathanestes@gmail.com>
2018-08-30 21:19:14 -0600

    added create, fixed destroy

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,5 @@
 class NotesController < ApplicationController
+
   def index
     @notes = Note.all
   end
@@ -12,28 +13,41 @@ class NotesController < ApplicationController
     #button to new.html.erb
   end
 
-
-def edit
-  @note = Note.find(params[:id])
-end
-
-def update
-  @note = Note.find(params[:id])
-
-  if @note.update(notes_params)
-    redirect_to notes_path
-  else
-    render :edit
+  def edit
+    @note = Note.find(params[:id])
   end
-end
 
+  def update
+    @note = Note.find(params[:id])
+
+    if @note.update(notes_params)
+    redirect_to notes_path
+    else
+    render :edit
+    end
+  end
+
+  def destroy
+    Note.find(params[:id]).destroy
+    redirect_to notes_path
+  end
+
+
+  def create #need a freaking create button...?
+    @note = Note.new(notes_params)
+
+    if @note.save
+      redirect_to note_path(@note.id)
+    else
+      render :new
+    end
+  end
 
 
 private
 
-def notes_params
+  def notes_params
   params.require(:note).permit(:title, :body)
-end
-
+  end
 
 end
